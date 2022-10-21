68821a32948ed2ad4f351c3db9e9ae85f9c13f8c
Auxburger <darius.augsburger@redpoint.teseon.com>
2022-10-21 02:01:10 +0200

    initialize project

---
+++ js/index.js
@@ -0,0 +1,514 @@
+const counterDOM = document.getElementById("counter");
+const endDOM = document.getElementById("end");
+
+const scene = new THREE.Scene();
+
+const distance = 500;
+const camera = new THREE.OrthographicCamera(
+  window.innerWidth / -2,
+  window.innerWidth / 2,
+  window.innerHeight / 2,
+  window.innerHeight / -2,
+  0.1,
+  10000
+);
+
+// for random number generation with seed
+function cyrb128(str) {
+  let h1 = 1779033703,
+    h2 = 3144134277,
+    h3 = 1013904242,
+    h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  return [
+    (h1 ^ h2 ^ h3 ^ h4) >>> 0,
+    (h2 ^ h1) >>> 0,
+    (h3 ^ h1) >>> 0,
+    (h4 ^ h1) >>> 0,
+  ];
+}
+
+function mulberry32(a) {
+  return function () {
+    var t = (a += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
+}
+
+const seed = cyrb128("seed");
+var rand = mulberry32(seed[0]); // our random number generator
+
+camera.rotation.x = (50 * Math.PI) / 180;
+camera.rotation.y = (20 * Math.PI) / 180;
+camera.rotation.z = (10 * Math.PI) / 180;
+
+const initialCameraPositionY = -Math.tan(camera.rotation.x) * distance;
+const initialCameraPositionX =
+  Math.tan(camera.rotation.y) *
+  Math.sqrt(distance ** 2 + initialCameraPositionY ** 2);
+camera.position.y = initialCameraPositionY;
+camera.position.x = initialCameraPositionX;
+camera.position.z = distance;
+
+const zoom = 1.5;
+
+const chickenSize = 15;
+
+const positionWidth = 42;
+const columns = 17;
+const boardWidth = positionWidth * columns;
+
+const stepTime = 200; // Miliseconds it takes for the chicken to take a step forward, backward, left or right
+
+let lanes;
+let currentLane;
+let currentColumn;
+
+let previousTimestamp;
+let startMoving;
+let moves;
+let stepStartTimestamp;
+
+const generateLanes = () =>
+  Array.from(Array(60).keys())
+    .map((index) => index - 30)
+    .map((index) => {
+      const lane = new Lane(index);
+      lane.mesh.position.y = index * positionWidth * zoom;
+      scene.add(lane.mesh);
+      return lane;
+    })
+    .filter((lane) => lane.index >= 0);
+
+const addLane = () => {
+  const index = lanes.length;
+  const lane = new Lane(index);
+  lane.mesh.position.y = index * positionWidth * zoom;
+  scene.add(lane.mesh);
+  lanes.push(lane);
+};
+
+const chicken = new Chicken();
+scene.add(chicken);
+
+hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+scene.add(hemiLight);
+
+const initialDirLightPositionX = -100;
+const initialDirLightPositionY = -100;
+dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(initialDirLightPositionX, initialDirLightPositionY, 200);
+dirLight.castShadow = true;
+dirLight.target = chicken;
+scene.add(dirLight);
+
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+var d = 2000;
+dirLight.shadow.camera.left = -d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = -d;
+
+// var helper = new THREE.CameraHelper( dirLight.shadow.camera );
+// var helper = new THREE.CameraHelper( camera );
+// scene.add(helper)
+
+backLight = new THREE.DirectionalLight(0x000000, 0.4);
+backLight.position.set(200, 200, 50);
+backLight.castShadow = true;
+scene.add(backLight);
+
+const laneTypes = ["forest"];
+const laneSpeeds = [2, 2.5, 3];
+const threeHeights = [20, 45, 60];
+
+const initializeValues = () => {
+  lanes = generateLanes();
+
+  currentLane = 0;
+  currentColumn = Math.floor(columns / 2);
+
+  previousTimestamp = null;
+
+  startMoving = false;
+  moves = [];
+  stepStartTimestamp;
+
+  chicken.position.x = 0;
+  chicken.position.y = 0;
+
+  camera.position.y = initialCameraPositionY;
+  camera.position.x = initialCameraPositionX;
+
+  dirLight.position.x = initialDirLightPositionX;
+  dirLight.position.y = initialDirLightPositionY;
+};
+
+initializeValues();
+
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function Texture(width, height, rects) {
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, width, height);
+  context.fillStyle = "rgba(0,0,0,0.6)";
+  rects.forEach((rect) => {
+    context.fillRect(rect.x, rect.y, rect.w, rect.h);
+  });
+  return new THREE.CanvasTexture(canvas);
+}
+
+function Three() {
+  const three = new THREE.Group();
+
+  const trunk = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(15 * zoom, 15 * zoom, 20 * zoom),
+    new THREE.MeshPhongMaterial({ color: 0x4d2926, flatShading: true })
+  );
+  trunk.position.z = 10 * zoom;
+  trunk.castShadow = true;
+  trunk.receiveShadow = true;
+  three.add(trunk);
+
+  height = threeHeights[Math.floor(Math.random() * threeHeights.length)];
+
+  const crown = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(30 * zoom, 30 * zoom, height * zoom),
+    new THREE.MeshLambertMaterial({ color: 0x7aa21d, flatShading: true })
+  );
+  crown.position.z = (height / 2 + 20) * zoom;
+  crown.castShadow = true;
+  crown.receiveShadow = false;
+  three.add(crown);
+
+  return three;
+}
+
+function Chicken() {
+  const chicken = new THREE.Group();
+
+  const body = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(
+      chickenSize * zoom,
+      chickenSize * zoom,
+      20 * zoom
+    ),
+    new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true })
+  );
+  body.position.z = 10 * zoom;
+  body.castShadow = true;
+  body.receiveShadow = true;
+  chicken.add(body);
+
+  const rowel = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(2 * zoom, 4 * zoom, 2 * zoom),
+    new THREE.MeshLambertMaterial({ color: 0xf0619a, flatShading: true })
+  );
+  rowel.position.z = 21 * zoom;
+  rowel.castShadow = true;
+  rowel.receiveShadow = false;
+  chicken.add(rowel);
+
+  return chicken;
+}
+
+function Grass() {
+  const grass = new THREE.Group();
+
+  const createSection = (color) =>
+    new THREE.Mesh(
+      new THREE.BoxBufferGeometry(
+        boardWidth * zoom,
+        positionWidth * zoom,
+        3 * zoom
+      ),
+      new THREE.MeshPhongMaterial({ color })
+    );
+
+  const middle = createSection(0xbaf455);
+  middle.receiveShadow = true;
+  grass.add(middle);
+
+  const left = createSection(0x99c846);
+  left.position.x = -boardWidth * zoom;
+  grass.add(left);
+
+  const right = createSection(0x99c846);
+  right.position.x = boardWidth * zoom;
+  grass.add(right);
+
+  grass.position.z = 1.5 * zoom;
+  return grass;
+}
+
+function Outback() {
+  const grass = new THREE.Group();
+
+  const createSection = (color) =>
+    new THREE.Mesh(
+      new THREE.BoxBufferGeometry(
+        boardWidth * zoom,
+        positionWidth * zoom,
+        3 * zoom
+      ),
+      new THREE.MeshPhongMaterial({ color })
+    );
+
+  const middle = createSection(0x99c846);
+  grass.add(middle);
+
+  const left = createSection(0x99c846);
+  left.position.x = -boardWidth * zoom;
+  grass.add(left);
+
+  const right = createSection(0x99c846);
+  right.position.x = boardWidth * zoom;
+  grass.add(right);
+
+  grass.position.z = 1.5 * zoom;
+  return grass;
+}
+
+function Lane(index) {
+  this.index = index;
+  if (index === 0) {
+    this.type = "field";
+  }
+  this.type =
+    index < 0 || index > 20
+      ? "outback"
+      : laneTypes[Math.floor(rand() * laneTypes.length)];
+
+  switch (this.type) {
+    case "field": {
+      this.type = "field";
+      this.mesh = new Grass();
+      break;
+    }
+    case "outback": {
+      this.type = "outback";
+      this.mesh = new Outback();
+      break;
+    }
+    case "forest": {
+      this.mesh = new Grass();
+
+      this.occupiedPositions = new Set();
+      this.threes = [1, 2, 3, 4].map(() => {
+        const three = new Three();
+        let position = 0;
+        do {
+          position = Math.floor(rand() * columns);
+        } while (this.occupiedPositions.has(position));
+        this.occupiedPositions.add(position);
+        three.position.x =
+          (position * positionWidth + positionWidth / 2) * zoom -
+          (boardWidth * zoom) / 2;
+        this.mesh.add(three);
+        return three;
+      });
+      break;
+    }
+  }
+}
+
+
+window.addEventListener("keydown", (event) => {
+  // if one still likes to run around :)
+  if (event.keyCode == "38") {
+    // up arrow
+    move("forward");
+  } else if (event.keyCode == "40") {
+    // down arrow
+    move("backward");
+  } else if (event.keyCode == "37") {
+    // left arrow
+    move("left");
+  } else if (event.keyCode == "39") {
+    // right arrow
+    move("right");
+  }
+});
+
+function goForward() {
+  move("forward");
+}
+function goLeft() {
+  move("left");
+}
+function goRight() {
+  move("right");
+}
+
+function move(direction) {
+  const finalPositions = moves.reduce(
+    (position, move) => {
+      switch (move) {
+        case "forward":
+          return { lane: position.lane + 1, column: position.column };
+        case "backward":
+          return { lane: position.lane - 1, column: position.column };
+        case "left":
+          return { lane: position.lane, column: position.column - 1 };
+        case "right":
+          return { lane: position.lane, column: position.column + 1 };
+      }
+    },
+    { lane: currentLane, column: currentColumn }
+  );
+
+  if (direction === "forward") {
+    if (finalPositions.lane === 20) return;
+    if (
+      lanes[finalPositions.lane + 1].type === "forest" &&
+      lanes[finalPositions.lane + 1].occupiedPositions.has(
+        finalPositions.column
+      )
+    )
+      return;
+    if (!stepStartTimestamp) startMoving = true;
+    addLane();
+  } else if (direction === "backward") {
+    if (finalPositions.lane === 0) return;
+    if (
+      lanes[finalPositions.lane - 1].type === "forest" &&
+      lanes[finalPositions.lane - 1].occupiedPositions.has(
+        finalPositions.column
+      )
+    )
+      return;
+    if (!stepStartTimestamp) startMoving = true;
+  } else if (direction === "left") {
+    if (finalPositions.column === 0) return;
+    if (
+      lanes[finalPositions.lane].type === "forest" &&
+      lanes[finalPositions.lane].occupiedPositions.has(
+        finalPositions.column - 1
+      )
+    )
+      return;
+    if (!stepStartTimestamp) startMoving = true;
+  } else if (direction === "right") {
+    if (finalPositions.column === columns - 1) return;
+    if (
+      lanes[finalPositions.lane].type === "forest" &&
+      lanes[finalPositions.lane].occupiedPositions.has(
+        finalPositions.column + 1
+      )
+    )
+      return;
+    if (!stepStartTimestamp) startMoving = true;
+  }
+  moves.push(direction);
+}
+
+function animate(timestamp) {
+  requestAnimationFrame(animate);
+
+  previousTimestamp = timestamp;
+
+  if (startMoving) {
+    stepStartTimestamp = timestamp;
+    startMoving = false;
+  }
+
+  if (stepStartTimestamp) {
+    const moveDeltaTime = timestamp - stepStartTimestamp;
+    const moveDeltaDistance =
+      Math.min(moveDeltaTime / stepTime, 1) * positionWidth * zoom;
+    const jumpDeltaDistance =
+      Math.sin(Math.min(moveDeltaTime / stepTime, 1) * Math.PI) * 8 * zoom;
+
+    let direction = 1;
+    switch (moves[0]) {
+      case "backward": {
+        direction = -1;
+      }
+      case "forward": {
+        const positionY =
+          currentLane * positionWidth * zoom + moveDeltaDistance * direction;
+        camera.position.y = initialCameraPositionY + positionY;
+        dirLight.position.y = initialDirLightPositionY + positionY;
+        chicken.position.y = positionY;
+        break;
+      }
+      case "left": {
+        direction = -1;
+      }
+      case "right": {
+        const positionX =
+          (currentColumn * positionWidth + positionWidth / 2) * zoom -
+          (boardWidth * zoom) / 2 +
+          moveDeltaDistance * direction;
+        camera.position.x = initialCameraPositionX + positionX;
+        dirLight.position.x = initialDirLightPositionX + positionX;
+        chicken.position.x = positionX;
+        break;
+      }
+    }
+    chicken.position.z = jumpDeltaDistance;
+
+    // Once a step has ended
+    if (moveDeltaTime > stepTime) {
+      switch (moves[0]) {
+        case "forward": {
+          currentLane++;
+          counterDOM.innerHTML = currentLane;
+          break;
+        }
+        case "backward": {
+          currentLane--;
+          counterDOM.innerHTML = currentLane;
+          break;
+        }
+        case "left": {
+          currentColumn--;
+          break;
+        }
+        case "right": {
+          currentColumn++;
+          break;
+        }
+      }
+      moves.shift();
+      // If more steps are to be taken then restart counter otherwise stop stepping
+      stepStartTimestamp = moves.length === 0 ? null : timestamp;
+    }
+  }
+
+  // Hit test
+  if (currentLane === 20) {
+    endDOM.style.visibility = "visible";
+  }
+  renderer.render(scene, camera);
+}
+
+requestAnimationFrame(animate);
+
+// TODO reach the goal by using a loop and the methods goForward, goRight and goLeft
+// ...
+
